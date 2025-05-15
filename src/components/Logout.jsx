47f241ffb3b29f39215f37f3a55788ce5bb5e91a
Jay-sorani835 <jaysorani835@gmail.com>
2025-05-15 10:55:39 +0530

    commit

---
+++ src/components/Logout.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Logout() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const logout = async () => {
+      try {
+        await axios.post("http://localhost:4000/api/admin/logout", null, {
+          withCredentials: true,
+        });
+        navigate("/");
+      } catch (err) {
+        console.error("Logout failed", err);
+        alert("Failed to logout");
+      }
+    };
+    logout();
+  }, [navigate]);
+
+  return <div>Logging out...</div>;
+}
+
+export default Logout;
